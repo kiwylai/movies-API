685eab45a74b7bf7a9603d13bca068b7f8035790
Kiwy <kiwylai@gmial.com>
2024-11-19 19:33:41 +0100

    Refactor Movie.java: Update reviewsIds type and annotation

--- movies/movies/src/main/java/dev/kiwy/movies/Movie.java
+++ movies/movies/src/main/java/dev/kiwy/movies/Movie.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
 
 import java.util.List;
 @Document(collection = "movies")
@@ -32,6 +32,6 @@ public class Movie {
 
     private List<String> backdrops;
 
-    @DocumentReference
+
     private List<String> reviewsIds;
 }
