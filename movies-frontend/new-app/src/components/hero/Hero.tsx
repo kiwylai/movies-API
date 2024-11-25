05fed5a0293d52d270a082864bb8ca6dbf1224a3
Kiwy <kiwylai@gmial.com>
2024-11-25 23:51:31 +0100

    create Hero.tsx

---
+++ movies-frontend/new-app/src/components/hero/Hero.tsx
@@ -0,0 +1,33 @@
+import "./Hero.css";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+
+const Hero: React.FC = ({ movies }) => {
+  return (
+    <div className="movie-carousel-sontainer">
+      <Carousel>
+        {movies?.map((movie) => {
+          return (
+            <Paper>
+              <div className="movie-card-container">
+                <div className="movie-card">
+                  <div className="movie-detail">
+                    <div className="movie-poster">
+                      <img src={movie.poster} />
+                    </div>
+                    <div className="movie-title">
+                      <h4></h4>
+                      {movie.title}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Paper>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Hero;
