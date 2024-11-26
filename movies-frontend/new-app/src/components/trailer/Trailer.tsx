d8f0915d8128f3dbcb95cd00852147587ab1d0b1
Kiwy <kiwylai@gmial.com>
2024-11-26 16:03:46 +0100

    create Trailer.tsx and import to App.tsx

---
+++ movies-frontend/new-app/src/components/trailer/Trailer.tsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import "./Trailer.css";
+import React from "react";
+
+interface TrailerParams {
+  ytTrailerId?: string;
+}
+
+const Trailer: React.FC = () => {
+  const params = useParams<TrailerParams>();
+  const key = params.ytTrailerId;
+
+  return (
+    <div className="react-player-container">
+      {key ? (
+        <ReactPlayer
+          controls={true}
+          playing={true}
+          url={`https://www.youtube.com/watch?v=${key}`}
+          width="100%"
+          height="100%"
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default Trailer;
