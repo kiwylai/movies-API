2ecd5241c796e2b67b9be1881afb12ddb20cbd88
Kiwy <kiwylai@gmial.com>
2024-11-26 21:11:23 +0100

    import trailer and review

--- movies-frontend/new-app/src/App.tsx
+++ movies-frontend/new-app/src/App.tsx
@@ -6,17 +6,45 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
+
+interface Movie {
+  imdbId: string;
+  title: string;
+  poster: string;
+  backdrops: string[];
+  trailerLink: string;
+  reviews: Review[];
+}
+
+interface Review {
+  body: string;
+}
 
 function App() {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState<Movie[] | undefined>(undefined);
+  const [movie, setMovie] = useState<Movie | undefined>(undefined);
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   const getMovies = async () => {
     try {
-      const response = await api.get("/api/v1/movies");
-
+      const response = await api.get<{ data: Movie[] }>("/api/v1/movies");
       setMovies(response.data);
     } catch (err) {
-      console.log(err);
+      console.error(err);
+    }
+  };
+
+  const getMovieData = async (movieId: string) => {
+    try {
+      const response = await api.get<{ data: Movie }>(
+        `/api/v1/movies/${movieId}`
+      );
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -28,9 +56,20 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home movies={movies} />}></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+        <Route path="*" element={<Layout />}>
+          <Route index element={<Home movies={movies} />} />
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          />
         </Route>
       </Routes>
     </div>
